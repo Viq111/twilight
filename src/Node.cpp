cea4bff6ef25cfebd3e065344e9631d2873f8823
Aur�lien Bobey <aurelien.bobey@student.ecp.fr>
2015-01-25 02:57:14 +0100

    Classe Node provisoire et simplifiee pour créer des graphes et tester une implémentation de l'algo alpha-beta

---
+++ src/Node.cpp
@@ -0,0 +1,34 @@
+#include "Node.h"
+#include <stdlib.h>
+#include <algorithm>
+
+Node::Node(int parentScore)
+{
+    score = parentScore + rand() % 100 - 50;
+}
+
+int Node::addChildren(Node* child)
+{
+    children.push_back(child);
+    return 0;
+}
+
+Node::~Node()
+{
+    if (children.size() > 0){
+        for (int i = 0; i < children.size(); i++){
+            delete(children[i]);
+        }
+    }
+}
+
+bool comparaisonInverse(Node* first, Node* second)
+{
+    return (first->getScore() > second->getScore());
+}
+
+std::vector<Node*> Node::getSortedChildren()
+{
+    std::sort(children.begin(), children.begin(), comparaisonInverse);
+    return children;
+}
---
+++ src/Node.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+
+class Node
+{
+    std::vector<Node*> children;
+    int score;
+    int alpha;
+    int beta;
+public:
+    Node(int parentScore);
+    ~Node();
+    int addChildren(Node* child);
+    std::vector<Node*> getSortedChildren();
+    int getScore(){ return score; };
+    int getAlpha() { return alpha; };
+    int getBeta(){ return beta; };
+};
+

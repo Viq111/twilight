8e0aaddcbc725237c73205048e3e8713b91d7b86
Damien Vielpeau <damien.vielpeau@gmail.com>
2015-02-27 12:41:17 +0100

    updated header file for generation of children, no implementation yet

--- src/GameState.h
+++ src/GameState.h
@@ -15,11 +15,12 @@
 #include <ctime>
 #include <memory>
 
-// Represent a group of creature with its position and size
+// Represent a group of creatures
 struct Group {
     int x;
     int y;
     int count;
+    bool hasAlreadyMoved;
 };
 
 enum Direction {
@@ -33,14 +34,24 @@ enum Direction {
     DownLeft
 };
 
+struct Move{
+     int x;
+     int y;
+     Direction dir;
+     int count;
+};
+
 // Represent a current board
 class GameState {
+
 public:
 	// Size is static because it should not change during the game
     static int n;
 	static int m;
+
     // Multiplication factor used in all scoring operations
     static int opsFactor;
+
     // Weights of the different parts of the score
     static int populationCountsWeight;
     static int humanProximityWeight;
@@ -54,22 +65,44 @@ public:
 				std::vector<Group> humans,
 				std::vector<Group> enemies);
 
-	const int alliesCount;
+    // Number of _groups_ for each race
+	int alliesCount;
 	const int humansCount;
-	const int enemiesCount;
-    // Utility method to sum the pop of groups
-    static int racePopulation(std::vector<Group> race);
-	std::vector<Group> allies;
+	int enemiesCount;
+
+    // Listes of groups of each race
+    std::vector<Group> allies;
     const std::vector<Group> humans;
     std::vector<Group> enemies;
 
+    // Utility method to sum the populations of groups
+    static int racePopulation(std::vector<Group> groupsOfRace);
+
+    // Generate children for alpha_beta algo
     std::shared_ptr<std::vector<std::shared_ptr<GameState>>> getChildren();
+
+    // How to access this state from his parents in alpha-beta tree
+    std::vector<Move> accessiblesViaMoves;
+
+    // Calculate score
     int getScore();
+
+    // For visualisation/debug purpose
     void print();
+
 private:
-    void tryDirection(std::shared_ptr<GameState> head, Direction dir, int index, std::shared_ptr<std::vector<std::shared_ptr<GameState>>> children, int popMax);
-    void getChildrenRecursive(int index, std::shared_ptr<GameState> head, std::shared_ptr<std::vector<std::shared_ptr<GameState>>> children);
-	void moveGroup(int index, Direction dir, int count);
+    // Useful to avoid impossible moves
+    std::vector<std::vector<bool>> departurePositions;
+    std::vector<std::vector<bool>> arrivalPositions;
+
+    // Compute all the possible evolution of a given group
+    // its a list of moves : for instance two guys go right, three go down
+    std::vector<std::vector<Move>> possibleEvolution (std::shared_ptr<Group> group);
+
+    // Apply an evolution to current GameState to create a new one
+    std::shared_ptr<GameState> applyEvolution(std::vector<std::vector<Move>> evolution);
+
+    // Utility method for the distance between 2 groups 
     int distance(const Group& group1, const Group& group2);
 };
 

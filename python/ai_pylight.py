1be446055b3ab2a02b35a1badb07fb7be6288bca
Vianney Tran <viq.dev@gmail.com>
2015-03-17 10:43:04 +0100

    Minmax

--- python/ai_pylight.py
+++ python/ai_pylight.py
@@ -21,7 +21,7 @@ from pprint import pprint
 ### GLOBALS ###
 ###############
 
-INITIAL_MINMAX_LEVEL = 1
+INITIAL_MINMAX_LEVEL = 2
 INITIAL_FAST_MINMAX_LEVEL = 1
 PENALITY_COEFF = 2
 DEBUG = False # In production, remove Debug so error are caught and a dumbed down version is used
